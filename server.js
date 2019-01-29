8c568ec72137a7892b45b5968434ed4aa8171a86
justi5ed <a2sidorov@gmail.com>
2019-01-29 19:04:59 +0300

    fixing db connection

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({ 
-	secret: process.env.SECRET_KEY || 'abcd1234',
+	secret: process.env.SECRET_KEY,
 	resave: true,
 	saveUninitialized: true,
 }));
