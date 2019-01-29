b3c7370965ec5e2b8bfb83d785169c7b47182f20
justi5ed <a2sidorov@gmail.com>
2019-01-29 16:26:04 +0300

    configured for Heroku

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ require('./config/passport')(passport);
 require('./config/scheduler');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true}));
